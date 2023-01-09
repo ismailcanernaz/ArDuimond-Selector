6c8b3f8a63e7e2866453d60c5155fa35c9fdc89d
İsmail Caner Naz <ismailcanernaz@aol.com>
2023-01-10 00:45:06 +0300

    Add files via upload

---
+++ ArDuimond-Selector/ArDuimond-Selector/ArDuimond-Selector.ino
@@ -0,0 +1,14 @@
+/*
+ * ArDuimond-Selector.ino
+ * ArDuimond Selector II
+ * Gem Stone & Diamond Tester
+ */
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
